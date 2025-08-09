1921489143bc6d3bd5332d91c03feaf7a87f3593
Katie Osias <145616229+KatieMew@users.noreply.github.com>
2025-08-08 22:25:35 -0400

    messing with phantomCamera and stylistic shaders

--- Main/camera.gd
+++ Main/camera.gd
@@ -1,13 +1,16 @@
-extends Camera3D
+extends Node3D
 
-@export var sway_amount: float = 0.1 
+@export var sway_amount: float = 0.1
 @export var sway_speed: float = 5.0 
+@export var sway_limit: float = 0.1
+@onready var phantom_camera_host = $Camera3D/PhantomCameraHost
 
 var target_offset := Vector3.ZERO
 var default_position := Vector3.ZERO
-
+var current_cam
 func _ready():
-	default_position = position
+	current_cam = phantom_camera_host.get_active_pcam()
+	default_position = current_cam.position
 
 func _process(delta):
 	var viewport_size = get_viewport().get_visible_rect().size
@@ -16,7 +19,6 @@ func _process(delta):
 	var offset_x = ((mouse_pos.x / viewport_size.x) - 0.5) * 2
 	var offset_y = ((mouse_pos.y / viewport_size.y) - 0.5) * 2
 
-	target_offset.x = -offset_x * sway_amount
-	target_offset.y = offset_y * sway_amount
-
-	position = position.lerp(default_position + target_offset, delta * sway_speed)
+	target_offset.x = clamp(offset_x * sway_amount, -sway_limit, sway_limit)
+	target_offset.y = clamp(-offset_y * sway_amount, -sway_limit, sway_limit)
+	current_cam.position = current_cam.position.lerp(default_position + target_offset, delta * sway_speed)
